0eaa0bdd425dd6d35d8f8c948ffb677dcd8e1b36
Tucker <tudge669@gmail.com>
2023-02-01 10:40:51 -0500

    commit tree cd4bf23559a2473700c300f568c26523a3b16f19

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction), error: "shoulda-matchers test string"}
+    validate :clickbait?
+
+
+    # def clickbait?
+    #     object = {string: ["Won't Believe","Secret Top [number]","Guess"]}
+    #     if title == object[string]
+    #         return true
+    #     end
+    # return true
+    # end
+    def clickbait?
+        mapping = ["Won't Believe","Secret Top [number]","Guess"]
+        if mapping.any? {|phrase| title.includes?(phrase)}
+        return 
+    else errors.add(:title, "title must be clickbait")
+        end
+        
+    end
 end
